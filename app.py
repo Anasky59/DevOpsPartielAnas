73b00a709fe73721c63c7bdb2246436832f2919a
Anas <Anasky59@gmail.com>
2025-02-26 12:34:28 +0100

    correction code

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
 def add(a, b):
-    """Ajoute deux nombres."""
     return a + b
 
 
-def multiply(x, y):
-    """Multiplie deux nombres."""
-    return x * y
+def multiply(a, b):
+    return a * b
 
 
-def divide(x, y):
-    """Divise x par y si y n'est pas zéro."""
-    if y != 0:
-        return x / y
+def divide(a, b):
+    if b == 0:
+        return None
+    return a / b
 
 
 def greet(name):
-    """Retourne un message de salutation."""
     if name == "":
         return "Hello, World!"
-    else:
-        return "Hello, " + name
+    return f"Hello,{name}"
